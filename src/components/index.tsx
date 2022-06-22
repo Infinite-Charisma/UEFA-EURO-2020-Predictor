f347719040478cecb07f474a8d3fcede9cbdf513
Infinite-Charisma <hotstuffjk0731@gmail.com>
2022-06-23 02:21:17 +0300

    update main

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.scss";
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export { default as Champions } from "./Champions";
+export { default as Group } from "./Group";
+export { default as GroupStage } from "./GroupStage";
+export { default as Knockouts } from "./Knockouts";
+export { default as ThirdPlaceLeague } from "./ThirdPlaceLeague";
+export { default as Flag } from "./Flag";
+export { default as Share } from "./Share";
+export { default as KnockoutMatch } from "./KnockoutMatch";
--- src/types/Rounds.ts
+++ src/types/Rounds.ts
@@ -7,4 +7,3 @@ export type Rounds =
   | "semis"
   | "final"
   | "champions";
-  
